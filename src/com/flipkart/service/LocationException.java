bda41137dcc7f929e11a11649fa9ec5ef42308af
tejas <tejeshwar.saini7@gmail.com>
2020-06-10 09:20:03 +0530

    edit

--- src/com/flipkart/service/LocationException.java
+++ src/com/flipkart/service/LocationException.java
@@ -3,6 +3,7 @@ package com.flipkart.service;
 public class LocationException extends Exception {
 	public LocationException(String location) {
 		System.out.println(location + "Cannot be Accepted For Delivery");
+		
 	}
 
 }
